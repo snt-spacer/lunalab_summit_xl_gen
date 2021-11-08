213082843210fe05d8e7ab62e74d1944ee2b5edf
Andrej Orsula <orsula.andrej@gmail.com>
2021-11-08 11:11:39 +0100

    Fix orientation goal in example (custom quat impl)
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- lunalab_summit_xl_gen_ign/examples/ex_moveit_follow_target.py
+++ lunalab_summit_xl_gen_ign/examples/ex_moveit_follow_target.py
@@ -4,9 +4,33 @@
 from geometry_msgs.msg import Pose, PoseStamped, Transform
 from moveit2 import MoveIt2Interface
 from rclpy.node import Node
+from typing import Tuple
 import rclpy
-
 import tf2_ros
+
+
+def quat_mul(quat_0: Tuple[float, float, float, float],
+             quat_1: Tuple[float, float, float, float],
+             xyzw: bool = True) -> Tuple[float, float, float, float]:
+    """
+    Multiply two quaternions
+    """
+    # TODO: Replace custom quaternion multiplication with tf2's do_transform() once it is implemented for use in Python.
+
+    if xyzw:
+        x0, y0, z0, w0 = quat_0
+        x1, y1, z1, w1 = quat_1
+        return (x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0,
+                -x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0)
+    else:
+        w0, x0, y0, z0 = quat_0
+        w1, x1, y1, z1 = quat_1
+        return (-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
+                x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0)
 
 
 class MoveItFollowTarget(Node):
@@ -36,19 +60,32 @@ class MoveItFollowTarget(Node):
         self.executor_.spin()
 
     def target_pose_callback(self, pose_msg):
+        """
+        Plan and execute kinematic path each time target's position is changed
+        """
 
         # Plan trajectory only if target was moved
         if self.previous_target_pose_ != pose_msg.pose:
-            # TODO: Do a proper transformation on target pose and not just position [Python interface for tf2 does not currently have this implemented - and its not worth implementing custom functions just for this example]
-            transform = self.lookup_transform_sync(target_frame="robot_j2s7s300_link_base",
+            # Set position goal (in world coordinate system)
+            self.moveit2_.set_position_goal([pose_msg.pose.position.x,
+                                             pose_msg.pose.position.y,
+                                             pose_msg.pose.position.z],
+                                            frame=pose_msg.header._frame_id)
+
+            # Set orientation goal (transformed into robot frame)
+            transform = self.lookup_transform_sync(target_frame=self.moveit2_.arm_base_link,
                                                    source_frame=pose_msg.header._frame_id)
-            self.moveit2_.set_pose_goal([pose_msg.pose.position.x+transform.translation.x,
-                                         pose_msg.pose.position.y+transform.translation.y,
-                                         pose_msg.pose.position.z+transform.translation.z],
-                                        [pose_msg.pose.orientation.x,
+            orientation_goal = quat_mul((pose_msg.pose.orientation.x,
                                          pose_msg.pose.orientation.y,
                                          pose_msg.pose.orientation.z,
-                                         pose_msg.pose.orientation.w])
+                                         pose_msg.pose.orientation.w),
+                                        (transform.rotation.x,
+                                         transform.rotation.y,
+                                         transform.rotation.z,
+                                         transform.rotation.w))
+            self.moveit2_.set_orientation_goal(orientation_goal)
+
+            # Plan and execute
             self.moveit2_.plan_kinematic_path()
             self.moveit2_.execute()
 
@@ -58,6 +95,9 @@ class MoveItFollowTarget(Node):
     def lookup_transform_sync(self,
                               target_frame: str,
                               source_frame: str) -> Transform:
+        """
+        Lookup transform from `source_frame` to `target_frame` using tf2
+        """
 
         while rclpy.ok():
             if self.tf2_buffer_.can_transform(target_frame=target_frame,
