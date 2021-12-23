4c0f7cbc1d493a7e90baff8782be1281c838d64a
Andrej Orsula <orsula.andrej@gmail.com>
2021-12-23 18:51:01 +0100

    Clean-up scripts
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- lunalab_summit_xl_gen_description/scripts/xacro2urdf.bash
+++ lunalab_summit_xl_gen_description/scripts/xacro2urdf.bash
@@ -2,39 +2,39 @@
 # This script converts xacro (URDF variant) into URDF for `lunalab_summit_xl_gen_description` package
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-XACRO_PATH="$(dirname ${SCRIPT_DIR})/urdf/lunalab_summit_xl_gen.urdf.xacro"
-URDF_PATH="$(dirname ${SCRIPT_DIR})/urdf/lunalab_summit_xl_gen.urdf"
+XACRO_PATH="$(dirname "${SCRIPT_DIR}")/urdf/lunalab_summit_xl_gen.urdf.xacro"
+URDF_PATH="$(dirname "${SCRIPT_DIR}")/urdf/lunalab_summit_xl_gen.urdf"
 
 # Arguments for xacro
-XACRO_ARGS="
-name:=lunalab_summit_xl_gen
-prefix:=robot_
-safety_limits:=true
-safety_soft_limit_margin:=0.17453293
-safety_k_position:=20
-collision_chassis:=true
-collision_wheels:=true
-collision_arm:=true
-collision_gripper:=true
-high_quality_mesh:=true
-mimic_gripper_joints:=false
-ros2_control:=true
-ros2_control_plugin:=ignition
-ros2_control_command_interface:=effort
-gazebo_preserve_fixed_joint:=false
-gazebo_self_collide_fingers:=true
-gazebo_diff_drive:=true
-gazebo_joint_trajectory_controller:=false
-gazebo_joint_state_publisher:=false
-gazebo_pose_publisher:=true
-"
+XACRO_ARGS=(
+    name:=lunalab_summit_xl_gen
+    prefix:=robot_
+    safety_limits:=true
+    safety_soft_limit_margin:=0.17453293
+    safety_k_position:=20
+    collision_chassis:=true
+    collision_wheels:=true
+    collision_arm:=true
+    collision_gripper:=true
+    high_quality_mesh:=true
+    mimic_gripper_joints:=false
+    ros2_control:=true
+    ros2_control_plugin:=ignition
+    ros2_control_command_interface:=effort
+    gazebo_preserve_fixed_joint:=false
+    gazebo_self_collide_fingers:=true
+    gazebo_diff_drive:=true
+    gazebo_joint_trajectory_controller:=false
+    gazebo_joint_state_publisher:=false
+    gazebo_pose_publisher:=true
+)
 
 # Remove old URDF file
-rm ${URDF_PATH} 2>/dev/null
+rm "${URDF_PATH}" 2>/dev/null
 
 # Process xacro into URDF
-xacro ${XACRO_PATH} ${XACRO_ARGS} -o ${URDF_PATH} &&
-    echo "Created new '${URDF_PATH}'"
+xacro "${XACRO_PATH}" "${XACRO_ARGS[@]}" -o "${URDF_PATH}" &&
+echo "Created new ${URDF_PATH}"
 
 # Add to stating area
-git add ${URDF_PATH}
+git add "${URDF_PATH}"
