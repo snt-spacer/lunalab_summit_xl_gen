a4b360a38a286259c8286b5293e971474e8ca41d
Andrej Orsula <orsula.andrej@gmail.com>
2022-01-12 13:26:32 +0100

    Add PBR material for rubber wheels
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- lunalab_summit_xl_gen_description/scripts/xacro2sdf.bash
+++ lunalab_summit_xl_gen_description/scripts/xacro2sdf.bash
@@ -36,7 +36,7 @@ rm "${SDF_PATH}" 2>/dev/null
 
 # Process xacro into URDF, then convert URDF to SDF and edit the SDF to use relative paths for meshes
 xacro "${XACRO_PATH}" "${XACRO_ARGS[@]}" -o "${TMP_URDF_PATH}" &&
-ign sdf -p "${TMP_URDF_PATH}" | sed 's/model:\/\/lunalab_summit_xl_gen_description\///g' >"${SDF_PATH}" &&
+ign sdf -p "${TMP_URDF_PATH}" | sed 's/model:\/\/lunalab_summit_xl_gen_description\///g' | sed 's/package:\/\/lunalab_summit_xl_gen_description\///g' >"${SDF_PATH}" &&
 echo "Created new ${SDF_PATH}"
 
 # Remove temporary URDF file
